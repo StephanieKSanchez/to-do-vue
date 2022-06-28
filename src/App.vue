d8569520930870c38b877120482f4ccc68be87e5
Stephanie Sanchez <stephk006@gmail.com>
2022-06-28 13:38:54 -0400

    corrected form closing tag

--- src/App.vue
+++ src/App.vue
@@ -34,39 +34,40 @@ onMounted(() => {
     <section class="create-todo">
       <h3>CREATE A TODO</h3>
 
-      <form @submit.prevent="addTodo"></form>
-      <h4>What's on your todo list?</h4>
-      <input
-        type="text"
-        placeholder="e.g. make a video"
-        v-model="input_content"
-      />
-      <h4>Pick a category</h4>
+      <form @submit.prevent="addTodo">
+        <h4>What's on your todo list?</h4>
+        <input
+          type="text"
+          placeholder="e.g. make a video"
+          v-model="input_content"
+        />
+        <h4>Pick a category</h4>
 
-      <div class="options">
-        <label>
-          <input
-            type="radio"
-            name="category"
-            id="catgory1"
-            value="business"
-            v-model="input_category"
-          />
-          <span class="bubble business"></span>
-          <div>Business</div>
-        </label>
-        <label>
-          <input
-            type="radio"
-            name="category"
-            id="catgory1"
-            value="personal"
-            v-model="input_category"
-          />
-          <span class="bubble personal"></span>
-          <div>Personal</div>
-        </label>
-      </div>
+        <div class="options">
+          <label>
+            <input
+              type="radio"
+              name="category"
+              id="catgory1"
+              value="business"
+              v-model="input_category"
+            />
+            <span class="bubble business"></span>
+            <div>Business</div>
+          </label>
+          <label>
+            <input
+              type="radio"
+              name="category"
+              id="catgory1"
+              value="personal"
+              v-model="input_category"
+            />
+            <span class="bubble personal"></span>
+            <div>Personal</div>
+          </label>
+        </div>
+      </form>
     </section>
   </main>
 </template>
